328a0e2bbb6038473c7e298cb9f1d11b4e92a115
FFLAB <riggio.francesco@gmail.com>
2020-03-10 15:00:12 +0100

    Create main-stateless.dart

---
+++ flutter_app_layout_lesson/lib/main-stateless.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // va messo qui
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Charlie e Sissy',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                        fontSize: 20
+                    ),
+                  ),
+                ),
+                Text(
+                  'Cani, Bassotti',
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      letterSpacing: 0.5,
+                      fontSize: 20
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Il bassotto è una razza canina di origine tedesca caratterizzata dall\'altezza proporzionalmente inferiore alla lunghezza; proprio la sua forma così curiosa lo rende particolarmente gradito ai bambini. Si tratta di un cane da caccia e da compagnia, selezionato per seguire gli animali selvatici nel sottobosco intricato, e affrontarli anche nelle tane sotterranee. Il nome originale è Dachshund termine di derivazione tedesca e fusione delle parole Dachs ("tasso") e Hund ("cane").',
+        softWrap: true,
+        style: TextStyle(
+            letterSpacing: 0.5,
+            fontSize: 18
+        ),
+      ),
+    );
+// qui sotto comando la posizione dei widget, in base ai loro nomi
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/bassotti.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
